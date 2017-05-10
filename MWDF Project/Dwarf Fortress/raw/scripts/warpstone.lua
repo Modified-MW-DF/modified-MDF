b51dd9ff61da7991bc95b4b720e561efe679a3c0
Amostubal <robertearlgriffin@gmail.com>
2017-05-10 05:41:27 -0500

    Warpstone fix
    This fix corrects issues that were occuring with warpstone gems trying
    to appear when digging out layer stone.  It removes 1 file no longer
    necessary.

--- MWDF Project/Dwarf Fortress/raw/scripts/warpstone.lua
+++ MWDF Project/Dwarf Fortress/raw/scripts/warpstone.lua
@@ -1,30 +1,22 @@
+--[[ 
+Warpstone.lua - this is a script that randomly produces a warpstone gem as 
+digging of a layer stone is completed.
+
+  this is a rewrite of a script from TESB by Dirst.
+  this rewrite was produced by Amostubal for DF 43.05 masterwork edition.
+     
+  Additional credits to other script writters include Putnam and Max for their
+  assistance with TESB and GetLayerMat() and GetVeinMat() are from Milo 
+  Christiansen's "Rubble Tile Material Getter", but with simpler inputs and 
+  outputs suited to this context.  Find the original module at 
+  http://www.bay12forums.com/smf/index.php?topic=150776
+--]]
+
 --[[
-Utility for The Earth Strikes Back! mod that allows the earth to actually 
-strike back.  Every time a layer stone tile is mined, it might turn out to 
-be "living stone" or a "hidden gem."
-
-Since this script needs to monitor all job completions, it *also* listens for
-completed buildings to turn Tributes into the correct subtype.  That 
-functionality incudes elements of Putnam's building/subtype-change script.
-
-	arguments
-		-living
-			Probability of layer stone turning out to be "living stone" that 
-			spawns an Awakened Stone or Wyrm (and possibly some Pet Rocks)
-			Defaults to .0005
-		-gem
-			Probability of layer stone turning out to be a "hidden gem"
-			Defaults to .002
-		-help
-			Show help
-
-Made by Dirst for The Earth Strikes Back! mod, but possible due to extensive 
-help from Putnam and Max.  GetLayerMat() and GetVeinMat() are from Milo 
-Christiansen's "Rubble Tile Material Getter", but with simpler inputs and 
-outputs suited to this context.  Find the original module at 
-http://www.bay12forums.com/smf/index.php?topic=150776
-]]
-
+ Left this in as it was in Dirst's original and I'm not sure how much 
+ of Milo's work remains after the fix and rewrite of this script.
+--]]
+  
 --[[
 Rubble Tile Material Getter DFHack Lua Module
 
@@ -49,178 +41,176 @@ misrepresented as being the original software.
 3. This notice may not be removed or altered from any source distribution.
 ]]
 
+local usage = [====[
+
+warpstone
+====================
+the script that processes whether a completed dig produces a warpstone.
+
+Usage::
+    - help
+        produces this help screen.
+    - layer X
+        sets the percentage chance of a gem appearing in any layer stone.
+        needs to be between 0 and 1.
+    - stable X
+        sets the percentage chance of a gem appearing in any stable warpstone.
+        needs to be between 0 and 1.
+
+if no argument is passed, this will produce a status of the mod.
+
+]====]
+
+local utils = require('utils')
+local warpstone_version= "1.00"
 
 function GetLayerMat(pos)
-	local region_info = dfhack.maps.getRegionBiome(dfhack.maps.getTileBiomeRgn(pos))
-	local map_block = dfhack.maps.ensureTileBlock(pos)
-	
-	local biome = df.world_geo_biome.find(region_info.geo_index)
-	
-	local layer_index = map_block.designation[pos.x%16][pos.y%16].geolayer_index
-	local layer_mat_index = biome.layers[layer_index].mat_index
-	
-	return layer_mat_index
+  local region_info = dfhack.maps.getRegionBiome(dfhack.maps.getTileBiomeRgn(pos))
+  local map_block = dfhack.maps.ensureTileBlock(pos)
+  
+  local biome = df.world_geo_biome.find(region_info.geo_index)
+  
+  local layer_index = map_block.designation[pos.x%16][pos.y%16].geolayer_index
+  local layer_mat_index = biome.layers[layer_index].mat_index
+  
+  return layer_mat_index
 end
 
 function GetVeinMat(pos)
-	local region_info = dfhack.maps.getRegionBiome(dfhack.maps.getTileBiomeRgn(pos))
-	local map_block = dfhack.maps.ensureTileBlock(pos)
-	
-	local events = {}
-	for _, event in ipairs(map_block.block_events) do
-		if getmetatable(event) == "block_square_event_mineralst" then
-			if dfhack.maps.getTileAssignment(event.tile_bitmask, pos.x, pos.y) then
-				table.insert(events, event)
-			end
-		end
-	end
-	
-	if #events == 0 then
-		return nil
-	end
-	
-	local event_priority = function(event)
-		if event.flags.cluster then
-			return 1
-		elseif event.flags.vein then
-			return 2
-		elseif event.flags.cluster_small then
-			return 3
-		elseif event.flags.cluster_one then
-			return 4
-		else
-			return 5
-		end
-	end
-	
-	local priority = events[1]
-	for _, event in ipairs(events) do
-		if event_priority(event) >= event_priority(priority) then
-			priority = event
-		end
-	end
-	
-	return priority.inorganic_mat
-end
-
--- Identify custom_type for Tribute
-local tribute_type
-for _,x in ipairs(df.global.world.raws.buildings.all) do
-	if x.code == "TESB_TRIBUTE" then tribute_type = x.id end
-end
-
-function specificWorkshop(workshop_id,stone)
-	local workshop = df.building.find(tonumber(workshop_id))
-	if workshop.custom_type ~= tribute_type then
-		print('Specified worshop is not a Tribute')
-	else
-		local tribute = "TESB_TRIBUTE_"..string.upper(stone)
-		for _,x in ipairs(df.global.world.raws.buildings.all) do
-			if x.code == tribute then ctype = x.id end
-		end
-		workshop.custom_type = ctype
-	end
+  local region_info = dfhack.maps.getRegionBiome(dfhack.maps.getTileBiomeRgn(pos))
+  local map_block = dfhack.maps.ensureTileBlock(pos)
+  
+  local events = {}
+  for _, event in ipairs(map_block.block_events) do
+    if getmetatable(event) == "block_square_event_mineralst" then
+      if dfhack.maps.getTileAssignment(event.tile_bitmask, pos.x, pos.y) then
+        table.insert(events, event)
+      end
+    end
+  end
+  
+  if #events == 0 then
+    return nil
+  end
+  
+  local event_priority = function(event)
+    if event.flags.cluster then
+      return 1
+    elseif event.flags.vein then
+      return 2
+    elseif event.flags.cluster_small then
+      return 3
+    elseif event.flags.cluster_one then
+      return 4
+    else
+      return 5
+    end
+  end
+  
+  local priority = events[1]
+  for _, event in ipairs(events) do
+    if event_priority(event) >= event_priority(priority) then
+      priority = event
+    end
+  end
+  
+  return priority.inorganic_mat
 end
 
 -- List of layer stones that can contain Living Stone or Hidden Gems
-stone_list = { "ANDESITE", "BASALT", "CHALK", "CHERT", "CLAYSTONE",
-	"CONGLOMERATE", "DACITE", "DIORITE", "DOLOMITE", "GABBRO", "GNEISS", 
-	"GRANITE", "LIMESTONE", "MARBLE", "MUDSTONE", "PHYLLITE", "QUARTZITE",
-	"RHYOLITE", "ROCK_SALT", "SANDSTONE", "SCHIST", "SHALE", "SILTSTONE",
-	"SLATE" }
+stone_list = { "ANDESITE", "BASALT", "CHALK",
+  "CHERT", "CLAYSTONE", "CONGLOMERATE",
+  "DACITE", "DIORITE", "DOLOMITE",
+  "GABBRO", "GNEISS", "GRANITE",
+  "LIMESTONE", "MARBLE", "MUDSTONE",
+  "PHYLLITE", "QUARTZITE", "RHYOLITE",
+  "ROCK_SALT", "SANDSTONE", "SCHIST",
+  "SHALE", "SILTSTONE", "SLATE",
+  "WARPSTONE_STABLE" }
 
--- List of Hidden Gems, in layer stone order
-gem_list = { "WARPSTONE" }
+-- List of digging jobs that can result in Living Stone or Hidden Gems
+digging_list = utils.invert({ "CarveDownwardStaircase", "CarveFortification", 
+  "CarveRamp", "CarveUpDownStaircase", "CarveUpwardStaircase", "Dig", 
+  "DigChannel", "RemoveStairs" })
 
-hiddenGem_list = {}
 caste_list = {}
-workshop_list = {}
 
 -- Lists indexed by material ID numbers
 for mat = 1, #stone_list do
-	local index = dfhack.matinfo.find(stone_list[mat]).index
-	hiddenGem_list[index] = gem_list[mat]
-	caste_list[index] = stone_list[mat]
-	workshop_list[index] = stone_list[mat]
+  local index = dfhack.matinfo.find(stone_list[mat]).index
+  caste_list[index] = stone_list[mat]
+  if debug then print("placed in caste_list[",index,"] <- ",stone_list[mat]) end
 end
 
-local utils = require('utils')
-
 local validArgs = validArgs or utils.invert({
-    'help',
-    'living',
-    'gem',
+  'help',
+  'layer',
+  'stable'
 })
 
 local args = utils.processArgs({...}, validArgs)
 
 if args.help then
-print([[scripts/tesb-onscreen-mining.lua
-Utility for The Earth Strikes Back! mod that allows the earth to actually 
-strike back.  Every time a layer stone tile is mined, it might turn out to 
-be "living stone" or a "hidden gem."
-
-Since this script needs to monitor all job completions, it *also* listens for
-completed buildings to turn Tributes into the correct subtype.
-
-arguments
-    -living
-        Probability of layer stone tile turning out to be "living stone" 
-        that spawns an Awakened Stone or Wyrm (and possibly some Pet Rocks)
-        Defaults to .002
-    -gem
-        Probability of layer stone turning out to be a "hidden gem"
-        Defaults to .005
-    -help
-        Show this help
-]])
-return
+  print(usage)
+  return
 end
 
-local living_prob = tonumber(args.living) or .002
-local gem_prob = tonumber(args.gem) or .005
+-- Load the config from the persistent tables.
+local config = dfhack.persistent.get("WARPSTONE/config")
+local gem_prob_layer
+local gem_prob_stable
+
+if config then
+  if config.value > warpstone_version then
+    dfhack.color(12)
+	dfhack.println("Cannot read configiration from version "..config.value..".")
+    return
+  end  -- load the config or replace with variables if given.
+  gem_prob_layer = tonumber(args.layer) or config.ints[1]/10000
+  gem_prob_stable = tonumber(args.stable) or config.ints[2]/10000
+else  -- no config so load variables given or set to 0.
+  gem_prob_layer = tonumber(args.layer) or 0
+  gem_prob_stable = tonumber(args.stable) or 0
+end
+
+--save the config in the persistent tables.
+dfhack.persistent.save({key="WARPSTONE/config",value=warpstone_version,ints={math.floor(gem_prob_layer*10000),math.floor(gem_prob_stable*10000)}})
+
 local rng = dfhack.random.new()
 
+-- cleaned this part to only that which is necessary for the script.
 jobCheck = require('plugins.eventful')
 jobCheck.onJobCompleted.warpstone1=function(job)
-	if job.job_type >= 3 and job.job_type <= 7 then -- Mining, digging stairs, etc.
-		local pos = job.pos
-		if caste_list[GetLayerMat(pos)] and (not GetVeinMat(pos)) and (not dfhack.maps.getTileBlock(pos).occupancy[pos.x%16][pos.y%16].item) then
-			if rng:drandom()<living_prob then
-				local command = "tesb-wake -caste " .. caste_list[GetLayerMat(pos)] .. " -location "
-				command = command .. "[ " .. pos.x .. " " .. pos.y .. " " .. pos.z .. " ]"
-				command = command .. " -miner " .. job.general_refs[0].unit_id
-				if rng:drandom()<living_prob^.5 then
-					command = command .. " -wyrm"
-				end
-				dfhack.run_command(command)
-			elseif rng:drandom()<gem_prob then
-				local gem = hiddenGem_list[GetLayerMat(pos)]
-				local gem_name = dfhack.matinfo.find(gem).material.state_name.Solid
-				dfhack.gui.makeAnnouncement(5,{false,false,false,true},pos,"You have struck " .. gem_name .. "!",6,1)
-				--dfhack.gui.showAnnouncement("You have struck " .. gem_name .. "!",15)
-				local command = "modtools/create-item -creator " .. job.general_refs[0].unit_id
-				command = command .. " -material \"INORGANIC:" .. gem .. "\""
-				command = command .. " -item ROUGH:NONE"
-				dfhack.run_command_silent(command)
-			end
-		end
-	elseif job.job_type == 68 then -- ConstructBuilding
-		-- check that it's a generic Tribute
-		local workshop = df.building.find(job.general_refs[0].building_id)
-		if workshop and workshop.construction_stage == 3 and workshop.mat_type == 0 and not workshop.design then
-			local workshop_mat = workshop_list[workshop.mat_index]  -- Will be nil if not on the list
-			if workshop.custom_type == tribute_type and workshop_mat then
-				workshop_mat_index = workshop.mat_index
-				if workshop.contained_items[0].item.mat_index == workshop_mat_index and 
-						workshop.contained_items[1].item.mat_index == workshop_mat_index and 
-						workshop.contained_items[2].item.mat_index == workshop_mat_index then
-					specificWorkshop(workshop.id,workshop_mat)
-				end
-			end
-		end
-	end
+  if digging_list[df.job_type[job.job_type]] then -- Will be nil if not on the list
+    local pos = job.pos
+    -- WARPSTONE_STABLE is a Vein, so we check for a vein of it. we can't check for layer, because layer tells you the layer of the floor no matter the vein.
+	if caste_list[GetVeinMat(pos)] and (not dfhack.maps.getTileBlock(pos).occupancy[pos.x%16][pos.y%16].item) then
+      if debug then print("we hit vein: ",GetVeinMat(pos)," which should be:",caste_list[GetVeinMat(pos)]) end
+      if rng:drandom()<gem_prob_stable then
+        dfhack.gui.makeAnnouncement(5,{false,false,false,true},pos,"You have struck warpstone!",6,1)
+        local command = "modtools/create-item -creator " .. job.general_refs[0].unit_id
+        command = command .. " -material INORGANIC:WARPSTONE -item ROUGH:NONE"
+        dfhack.run_command_silent(command)
+      end
+    -- the rest of the warpstone gems appear in layer stones.
+    elseif caste_list[GetLayerMat(pos)] and (not GetVeinMat(pos)) and (not dfhack.maps.getTileBlock(pos).occupancy[pos.x%16][pos.y%16].item) then
+      if debug then print("we hit layer: ",GetLayerMat(pos)," which should be:",caste_list[GetLayerMat(pos)]) end
+      if rng:drandom()<gem_prob_layer then
+        dfhack.gui.makeAnnouncement(5,{false,false,false,true},pos,"You have struck warpstone!",6,1)
+        local command = "modtools/create-item -creator " .. job.general_refs[0].unit_id
+        command = command .. " -material INORGANIC:WARPSTONE -item ROUGH:NONE"
+        dfhack.run_command_silent(command)
+      end
+    end
+  end
 end
+
+if not ... then
+  print('warpstone status:')
+  print('  chance of warpstone gem in layer stone: ', gem_prob_layer)
+  print('  chance of warpstone gem in stable warpstone: ', gem_prob_stable)
+end
+
 
 
 
